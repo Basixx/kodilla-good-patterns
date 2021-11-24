3cd2b248bdabb0291c290431c9444752d73119fd
Barbara Rutkowska <barbara_rutkowska.22@wp.pl>
2021-11-24 20:51:20 +0100

    13.1

---
+++ src/main/java/com/kodilla/good/patterns/challenges/GoodPatterns.java
@@ -0,0 +1,9 @@
+package com.kodilla.good.patterns.challenges;
+
+public class GoodPatterns {
+    public static void main(String[] args) {
+
+        MovieStore movieStore = new MovieStore();
+        System.out.println(movieStore);
+    }
+}
